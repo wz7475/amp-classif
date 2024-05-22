a5e3aa916223402d43387ed0b4ba8c0fa1d4815b
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-05-23 00:04:25 +0200

    feat: prediction from stdin args

--- classif/inference.py
+++ classif/inference.py
@@ -1,5 +1,10 @@
 from classif.ampscannerv2.api import predict_ampscanner
+from argparse import ArgumentParser
 
 if __name__ == "__main__":
     # predict_stm('resources/TEST.fasta', 'outputs')
-    predict_ampscanner('resources/TEST.fasta', 'outputs')
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input_path', type=str, required=True)
+    parser.add_argument('-o', '--output_path', type=str, required=True)
+    args = parser.parse_args()
+    predict_ampscanner(args.input_path, args.output_path)
